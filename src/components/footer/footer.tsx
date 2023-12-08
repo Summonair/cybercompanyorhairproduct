a67e9a4076087e028fd25550748bd66d2137f25d
talb <12tooltool@gmail.com>
2023-12-08 15:45:39 +0200

    added base forms

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return <p className="footer">
+    Made by <a href="https://github.com/Summonair/">@Summonair</a>,
+    Inspired by <a href="https://github.com/pixelastic/pokemonorbigdata">pokemonorbigdata  on GitHub</a>.
+  </p>
+}
+
+export default Footer;
