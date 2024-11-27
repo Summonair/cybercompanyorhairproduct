179df4c78793ba4dd0b145325f14b46db9904510
Tal Balash <12tooltool@gmail.com>
2024-11-27 12:05:06 +0200

    added prettier and fixes public url

--- src/components/QuestionForm/QuizEnded.tsx
+++ src/components/QuestionForm/QuizEnded.tsx
@@ -1,17 +1,12 @@
-import "./QuestionForm.css"
+import "./QuestionForm.css";
 
-const QuizEnded: React.FC<{ correct: number; total: number }> = ({
-  correct,
-  total,
-}) => {
+const QuizEnded: React.FC<{ correct: number; total: number }> = ({ correct, total }) => {
   return (
     <>
       <span>
         You got {correct}/{total} questions right
       </span>
-      <div className={correct === total && correct !== 0 ? "correct" : ""}>
-        {correct === total && correct !== 0 ? "Flawless Score!" : ""}
-      </div>
+      <div className={correct === total && correct !== 0 ? "correct" : ""}>{correct === total && correct !== 0 ? "Flawless Score!" : ""}</div>
     </>
   );
 };
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,11 @@
 import "./footer.css";
 
 const Footer = () => {
-  return <p className="footer">
-    Made by <a href="https://github.com/Summonair/">@Summonair</a>,
-    Inspired by <a href="https://github.com/pixelastic/pokemonorbigdata">pokemonorbigdata  on GitHub</a>.
-  </p>
-}
+  return (
+    <p className="footer">
+      Made by <a href="https://github.com/Summonair/">@Summonair</a>, Inspired by <a href="https://github.com/pixelastic/pokemonorbigdata">pokemonorbigdata on GitHub</a>.
+    </p>
+  );
+};
 
 export default Footer;
